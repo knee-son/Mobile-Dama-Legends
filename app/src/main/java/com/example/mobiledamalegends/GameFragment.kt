0905be60e4e2c9c68fcd0853241d4d7388223cc8
knee-son <okaokaloid@gmail.com>
2022-07-21 02:27:26 +0800

    tried to pop up the white piece but to no avail

--- app/src/main/java/com/example/mobiledamalegends/GameFragment.kt
+++ app/src/main/java/com/example/mobiledamalegends/GameFragment.kt
@@ -1,10 +1,11 @@
 package com.example.mobiledamalegends
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.mobiledamalegends.databinding.FragmentGameBinding
 
@@ -23,14 +24,43 @@ class GameFragment : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentGameBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        println( "successfully made gamefragment" )
+
+        var wpiece = binding.whitepiece
+        var wpiece_params = wpiece.layoutParams
+
+        wpiece.isFocusable = true
+        println ("is the piece focusable? " + wpiece.isFocusable)
+
+        wpiece.setOnClickListener{ wpiece.requestFocus(); println("clicked!")}
+
+        wpiece.setOnFocusChangeListener { view, b ->
+            wpiece.requestLayout()
+            when (b){
+                true -> { view.layoutParams.height = (wpiece_params.height * 1.2).toInt()
+                        view.layoutParams.width = (wpiece_params.width * 1.2).toInt() }
+                false -> { view.layoutParams.height = wpiece_params.height
+                        view.layoutParams.width = wpiece_params.width }
+            }
+            println( view.layoutParams )
+        }
+
+//        wpiece.setOnClickListener {
+//
+//            wpiece_clicked = !wpiece_clicked
+//            wpiece.requestLayout()
+//            when (wpiece_clicked){
+//                true -> wpiece.layoutParams.height =
+//                false -> wpiece.height = wpiece.height*1.2
+//            }
+//        }
 
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
