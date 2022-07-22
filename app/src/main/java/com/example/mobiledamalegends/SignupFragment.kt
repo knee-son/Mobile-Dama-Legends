d9a85004cfe9cad724902ffbf5c6de4e5e26896c
knee-son <okaokaloid@gmail.com>
2022-07-22 08:31:32 +0800

    added code for registering

--- app/src/main/java/com/example/mobiledamalegends/SignupFragment.kt
+++ app/src/main/java/com/example/mobiledamalegends/SignupFragment.kt
@@ -1,12 +1,19 @@
 package com.example.mobiledamalegends
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
-import com.example.mobiledamalegends.databinding.FragmentMenuBinding
+import com.example.mobiledamalegends.ProfileFragment.Companion.newInstance
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.example.mobiledamalegends.databinding.FragmentSignupBinding
 
 /**
@@ -40,6 +47,60 @@ class SignupFragment : Fragment() {
 
         binding.btnSignup.setOnClickListener {
             findNavController().navigate(R.id.action_SignupFragment_to_GameFragment)
+        }
+
+        binding.btnSignup.setOnClickListener {
+            when {
+                TextUtils.isEmpty(binding.inputEmail.text.toString().trim { it <= ' '}) -> {
+                    Toast.makeText(
+                        getActivity(),
+                        "Please enter email.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(binding.inputPassword.text.toString().trim { it <= ' '}) -> {
+                    Toast.makeText(
+                        getActivity(),
+                        "Please enter password.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                else -> {
+                    val email: String = binding.inputEmail.text.toString().trim { it <= ' '}
+                    val password: String = binding.inputPassword.text.toString().trim { it <= ' '}
+
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(
+                            OnCompleteListener<AuthResult> { task ->
+                                if (task.isSuccessful) {
+                                    val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                                    Toast.makeText(
+                                        getActivity(),
+                                        "You have been registered successfully!",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+                                    val intent =
+                                        Intent(getActivity(), SignupFragment::class.java)
+                                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    intent.putExtra("user_id", firebaseUser.uid)
+                                    intent.putExtra("email_id", email)
+                                    startActivity(intent)
+
+                                } else{
+                                    Toast.makeText(
+                                        getActivity(),
+                                        task.exception!!.message.toString(),
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                        )
+                }
+            }
         }
     }
 
